e4fcc7934d2469eb0c9f310bd4f974212ae5deee
Anastasiia Snihur <snihur.anastasiia@gmail.com>
2025-01-02 22:41:11 +0200

    check for id & name added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const inputs = document.querySelectorAll('input');
 
+  let idCounter = 1;
+
   inputs.forEach(input => {
     const label = document.createElement('label');
 
     label.classList.add('field-label');
 
+    if (!input.id) {
+      input.id = `input-${idCounter}`
+      idCounter++;
+    }
+
     label.setAttribute('for', input.id);
 
-    label.textContent = `${input.name}`.toUpperCase();
+    label.textContent = `${input.name || 'Field'}`.toUpperCase();
 
-    input.setAttribute('placeholder', `${input.name}`);
+    input.setAttribute('placeholder', `${input.name || 'Enter value'}`);
 
     const container = input.parentElement;
 
